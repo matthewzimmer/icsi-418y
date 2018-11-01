dc1935edee48c63e98936f75b0e1a09095b3e98a
Matthew Zimmer <m16r01@gmail.com>
2018-11-01 18:02:27 -0400

    Added command to enable "SQL safe mode" recommended by Django when I ran `python manage.py migrate`:
    https://docs.djangoproject.com/en/2.1/ref/databases/#mysql-sql-mode

--- scrapd/settings.py
+++ scrapd/settings.py
@@ -85,7 +85,9 @@ WSGI_APPLICATION = 'scrapd.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {},
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
         'NAME': get_env_variable('DATABASE_NAME'),
         'USER': get_env_variable('DATABASE_USER'),
         'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
