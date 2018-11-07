8ef096698b7354dbe6d0612bf7a1898a202d7209
Matthew Zimmer <m16r01@gmail.com>
2018-11-07 16:47:30 -0500

    Added cofiguration that will server all app.urls from the root URI.

--- csi418y/settings.py
+++ csi418y/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'j!4vfe!-o+95z++_w789b+$-!2-rn-adn9esdphcx*7)c9i7z%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["scrapd.us-east-1.elasticbeanstalk.com"]
+ALLOWED_HOSTS = [
+    "scrapd.us-east-1.elasticbeanstalk.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
