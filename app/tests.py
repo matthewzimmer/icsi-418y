14cd40009cdcc5e71b0c93955252a8f345cea7ec
Matthew Zimmer <3310088+matthewzimmer@users.noreply.github.com>
2018-11-06 23:27:21 -0500

    Travis CI Integration (#4)
    * Added .travis.yml from instructions:
    
    https://docs.travis-ci.com/user/tutorial/
    https://docs.travis-ci.com/user/languages/python/#specifying-python-versions
    https://docs.travis-ci.com/user/database-setup/#mysql

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.utils import timezone
+from app.models import Question
 
 # Create your tests here.
+class QuestionTestCase(TestCase):
+    def setUp(self):
+        Question.objects.create(question_text="How", pub_date=timezone.now())
+
+    def test_questions_can_be_asked(self):
+        question = Question.objects.get(question_text="How")
+        assert hasattr(question, "question_text")
