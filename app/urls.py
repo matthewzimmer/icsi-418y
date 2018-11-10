25d8c7ed1582facecea53d2645d24a0c76eb7ac1
Matthew Zimmer <m16r01@gmail.com>
2018-11-09 23:21:07 -0500

    Stubbed views.py methods and urls.

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('search', views.search, name='search'),
+    path('results', views.results, name='results'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def index(request):
-    return HttpResponse("Hello, world. This is my first Django view.")
+    return redirect('/login')
+
+
+def login(request):
+    return HttpResponse("TODO: Implement LOGIN UI.")
+
+
+def search(request):
+    return HttpResponse("TODO: Implement SEARCH UI.")
+
+
+def results(request):
+    return HttpResponse("TODO: Implement SEARCH RESULTS UI.")
