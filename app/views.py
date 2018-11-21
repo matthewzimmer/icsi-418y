7599059d715e8e8cb7c7f545c4aaec9d594b70f2
Matthew Zimmer <m16r01@gmail.com>
2018-11-20 21:18:56 -0500

    parsing comma-separated list of symbols and querying multiple. rendering ScrapeResults in results.html view.

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from app.models import Symbol, ScrapeResult
+
 
 @login_required
 def index(request):
@@ -27,4 +29,10 @@ def search(request):
 
 @login_required
 def results(request):
-    return render(request, 'results.html')
+    symbol_names = request.GET.get('symbols')
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+    symbols = Symbol.objects.filter(name__in=symbol_names.split(','))
+    symbol_ids = symbols.values_list('id', flat=True)
+    scrape_results = ScrapeResult.objects.filter(symbol_id__in=symbol_ids)
+    return render(request, 'results.html', {'GET': request.GET, 'scrape_results': scrape_results})
